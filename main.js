8dd43c9007ca7de2b2b3a47f33b95a699d8dae8c
mia <mia.can2021@gmail.com>
2021-07-14 20:59:25 -0700

    add function

--- main.js
+++ main.js
@@ -3,4 +3,35 @@ const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb
 
 const cities = [];
 
-fetch(endpoint);
+fetch(endpoint)
+.then(response => response.json())
+.then(data => cities.push(...data));
+
+function findMatches(wordToMatch, cities) {
+    return cities.filter(place => {
+        const regex = new RegExp(wordToMatch, 'gi');
+        return place.city.match(regex) || place.state.match(regex)
+    });
+}
+
+function displayMatches() {
+    const matchArray = findMatches(this.value, cities);
+    const html = matchArray.map(place => {
+        const regex = new RegExp(this.value, 'gi');
+        const cityName = place.city.replace(regex, `<span class ="h1">${this.value}</span>`);
+        const stateName = place.state.replace(regex, `<span class ="h1">${this.value}</span>`);
+        return `
+            <li>
+            <span class="name">${cityName}, ${stateName}</span>
+            <span class="population"> = Population: ${place.population}</span>
+            <span class="population">&Rank: ${place.rank}</span>
+            </li>
+        `;
+    }).join('');
+    suggestions.innerHTML = html;
+}
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
